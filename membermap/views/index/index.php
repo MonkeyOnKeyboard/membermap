8ec3d72123d2be2eb6d0962e6af7432d701c619b
MonkeyOnKeyboard <markuskrueger84@yahoo.de>
2021-04-27 19:17:40 +0200

    update view change php

--- membermap/views/index/index.php
+++ membermap/views/index/index.php
@@ -8,40 +8,64 @@
 <form class="form-horizontal" method="POST" action="">
 <?=$this->getTokenField() ?>
 
+<?php 
+                    $city_array = [];
+                    $my_array = [];
+                    $names_array = [];
+                    $out_array = [];
+                    foreach ($this->get('memberlocations') as $location) {
+                        
+                        if ($location->getCity() != "") {
+                            $name         =   $location->getName();
+                            $zip_code         =   $location->getZip_code();
+                            $country_code = $location->getCountry_code();
+                            $address			=	$location->getCity();
+                            $address			=	str_replace(array('ä','ü','ö','ß'), array('ae', 'ue', 'oe', 'ss'), $address );
+                            $address			=	preg_replace("/[^a-zA-Z0-9\_\s]/", "", $address);
+                            $address			=	strtolower($address);
+                            $address			=	str_replace( array(' ', '--'), array('-', '-'), $address );
+                            
+                            
+                            $city_array = [
+                                "names" => $name,
+                                "zip_code" => $zip_code,
+                                "address" => $address,
+                                "country_code" => $country_code,
+                                
+                                
+                            ];
+                            
+                            array_push($my_array, $city_array);
+                            
+                            $names_array = [
+                                
+                                "names" => $name,
+                                "city" => $address,
+                                
+                            ];
+                            
+                            array_push($out_array, $names_array);
+                            
+                        }
+                    }
+                    
+                    $js_json = json_encode($out_array, JSON_FORCE_OBJECT);
+             		  ?>
+
+
 
 <script type="text/javascript">
         window.onload = function() {
 
-        MQ.geocode().search([<?php 
-            $names_array = [];
-            $out_array = [];
-            foreach ($this->get('memberlocations') as $location) {
-                
-                if ($location->getCity() != "") {
-                    $name         =   $location->getName();
-                    $zip_code         =   $location->getZip_code();
-                    $country_code = $location->getCountry_code();
-                    $address			=	$location->getCity();
-                    $address			=	str_replace(array('ä','ü','ö','ß'), array('ae', 'ue', 'oe', 'ss'), $address );
-                    $address			=	preg_replace("/[^a-zA-Z0-9\_\s]/", "", $address);
-                    $address			=	strtolower($address);
-                    $address			=	str_replace( array(' ', '--'), array('-', '-'), $address );
-                    echo "'$zip_code $address $country_code'"; echo ", ";
-                    
-                    $names_array = [
-                        
-                        "names" => $name,
-                        "city" => $address,
-                        
-                    ];
-                    
-                    array_push($out_array, $names_array);
-                    
-                }
-            }
-            
-            $js_json = json_encode($out_array, JSON_FORCE_OBJECT);
-     		  ?>])
+        	
+    		
+        MQ.geocode().search([
+        		<?php 
+        		      foreach ($my_array as $city) {
+        		          echo "'$city[zip_code] $city[address] $city[country_code]'"; echo ", ";
+        		      }
+        		?>
+             ])
             .on('success', function(e) {
                 var results = e.result,
                     html = '',
