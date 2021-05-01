4f0e768ba07e4dd58f5da6aae9c91755bd986d63
Dennis Reilard <hdallo@web.de>
2021-05-01 21:13:02 +0200

    Anzeige Update

--- membermap/views/index/index.php
+++ membermap/views/index/index.php
@@ -2,13 +2,13 @@
     <?=$this->getTokenField() ?>
 
     <div align="center">
-        <br>
-        <br>
+        <?php if ($this->getUser()) { ?>
         <span style=" font-size: 30px;">
             <a href="<?= $this->getUrl(['action' => 'treat'])?>" title="<?= $this->getTrans('mapEntry')?>"><?= $this->getTrans('mapEntry')?></a>
         </span>
         <br>
         <br>
+        <?php } ?>
         <span style=" font-size: 30px;">
             <a href="<?= $this->getUrl(['action' => 'map'])?>" title="<?= $this->getTrans('mapView')?>"><?= $this->getTrans('mapView')?></a>
         </span>            	
